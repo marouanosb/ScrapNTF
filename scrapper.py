04c19f11261ccf2a05e155266bd27df73d1f1c34
unknown <marouanosb@gmail.com>
2023-11-29 22:37:06 +0100

    added getAllTrains query

--- scrapper.py
+++ scrapper.py
@@ -1,43 +1,76 @@
+import charset_normalizer
 import requests
 from bs4 import BeautifulSoup as bs
 import json
+import datetime
 
 sntfUrl = "https://www.sntf.dz/"
-departureStations = []
-arrivalStations = []
+stations = []
+trains = []
 
-#scrape the web page to extract all stations
-def scrape(url):
+
+#scrape the web page form to extract all stations
+def scrapeStations(url):
     sntf_page = requests.get(url).content
 
     soup = bs(sntf_page, "lxml")
+    stationsOptions = soup.find("select", id="gd")
 
-    departureOptions = soup.find("select", id="gd")
-    arrivalOptions = soup.find("select", id="ga")
-
-    for option in departureOptions.find_all("option"):
+    for option in stationsOptions.find_all("option"):
         gare = {"id": option["value"],
                "name": option.text}
-        departureStations.append(gare)
-    departureStations.pop(0)
+        stations.append(gare)
+
+     #delete the first element which is the select default text
+    stations.pop(0)
 
 
-    for option in arrivalStations.find_all("option"):
-        gare = {"id": option["value"],
-               "name": option.text}
-        arrivalStations.append(gare)
-    arrivalStations.pop(0)
+#scrape the result list web page to extract all trains
+def scrapeTrains(departureStationValue,arrivalStationValue,date):
 
-    #returns all stations as JSON
-    def getAllStations():
-        return json.dumps(departureStations)
+    #format the date to pass in the request format "yyyyMMdd"
+    strDate = str(date.year) + str(date.month) + str(date.day)
+    
+    trainsUrl = sntfUrl + f"index.php/component/sntf/?gd={departureStationValue}&ga={arrivalStationValue}&dd={strDate}&h1=0000&h2=2359&view=train"
+    trains_page = requests.get(trainsUrl).content
+
+    soup = bs(trains_page, "lxml")
+    trainsDetailsDivs = soup.find_all("div", class_="col-xs-7 mob_train_body")
+
+    #to remove the eventual extra spaces (\t\r\n) in the extracted text
+    characters_to_remove = {'\t': None, '\r': None, '\n': None}
+
+    for div in trainsDetailsDivs:
+        train = {"id": div.contents[1].text,
+                 "type": div.contents[3].text,
+                 "departureStation": div.contents[5].text,
+                 "departureTime": div.contents[7].text,
+                 "arrivalStation": div.contents[9].text,
+                 "arrivalTime": div.contents[11].text}
+        
+        #remove extra sspaces
+        for key in train:
+            train[key] = train[key].strip().translate(str.maketrans(characters_to_remove))
+
+        trains.append(train)
+
+
+#returns all stations as JSON
+def getAllStations():
+    scrapeStations(sntfUrl)
+
+    #"ensure_ascii=False" to disable converting special characters (é/à...etc) to ascii
+    return json.dumps(stations, ensure_ascii=False, indent=2)
+
 
     #returns list of trains of the given stations as JSON
-    def getTrains(departureStation, arrivalStation):
+def getAllTrains(departureStation, arrivalStation, date = datetime.date.today()):
+    scrapeTrains(departureStation,arrivalStation, date)
 
-        return
+    #"ensure_ascii=False" to disable converting special characters (é/à...etc) to ascii
+    return json.dumps(trains, ensure_ascii=False, indent=2)
     
 
 if __name__ == "__main__" :
-    scrape(sntfUrl)
+    print(getAllTrains("415","126",datetime.date.today()))
     
